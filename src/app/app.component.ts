3bad16f7eaa09ff1e6b4c67ba46d523d76683f12
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-12-06 21:51:42 +0000

    add div container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>compareit</h1>
-    <router-outlet></router-outlet>
+    <div class="container">
+      <h1>compareit</h1>
+      <router-outlet></router-outlet>
+    </div>
   `,
   styles: []
 })
