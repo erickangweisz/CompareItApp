8534a505a2e8a5e33e2b7f40d1f74ed09e28c662
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-12-07 00:10:57 +0000

    generate search-service

--- src/app/_services/search/search.service.ts
+++ src/app/_services/search/search.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  apiUrl = environment.apiURL;
+  
+  constructor(
+    private http: HttpClient,
+  ) {}
 
-  constructor() { }
+  getProducts() {
+    
+  }
 }
