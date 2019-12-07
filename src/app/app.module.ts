3403732b00b7854fc93a4ac5fc614b988798b71b
Pedro <pedrobusou@gmail.com>
2019-12-07 14:18:51 +0100

    Generate product-list-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { SearchFormComponent } from './_components/search-form/search-form.component';
 import { SearchComponent } from './search/search.component';
 import { ProductListComponent } from './_components/product-list/product-list.component';
+import { ProductListItemComponent } from './_components/product-list-item/product-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFormComponent,
     SearchComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductListItemComponent
   ],
   imports: [
     BrowserModule,
