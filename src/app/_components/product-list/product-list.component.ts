f7d15eb11b0e197adf3c2c7e01fb5887b6c10cca
Pedro <pedrobusou@gmail.com>
2019-12-07 14:52:01 +0100

    Prepare product-list for receiving array of products. Genereta product-list-items dynamically

--- src/app/_components/product-list/product-list.component.ts
+++ src/app/_components/product-list/product-list.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProductListComponent {
+  @Input() products;
 }
