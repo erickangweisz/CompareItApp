b927dc8b3a5d8121af78ae5908fe7fda73fa2bf3
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-12-07 00:47:58 +0000

    Load product-list on search page. Get url params

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,6 +10,17 @@ export class SearchComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+    const urlParams = location.search;
 
+    if (urlParams) {
+      console.log('Abemus params', urlParams);
+
+      //TODO: call api
+      
+      //TODO: fill app-product-list con los products que devuelva la api
+
+    } else {
+      console.log('No params bruh');
+    }
+  }
 }
