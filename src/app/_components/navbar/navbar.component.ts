d09aa4e312c48444b8a0261de154bcb7900a0ce8
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-03 10:29:27 +0000

    declare function toggleSidenav()

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  toggleSidenav() {}
 }
