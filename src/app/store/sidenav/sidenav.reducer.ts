ae4f5172feabcb8421a896e3e25ea7a78298fecb
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-04 22:59:03 +0000

    capitalize SidenavReducer function

--- src/app/store/sidenav/sidenav.reducer.ts
+++ src/app/store/sidenav/sidenav.reducer.ts
@@ -1,7 +1,7 @@
 import * as actionSidenav from './sidenav.actions';
 
-export function sidenavReducer(
-    state: boolean = false, action: actionSidenav.actions) {
+export function SidenavReducer(
+    state: boolean = true, action: actionSidenav.actions) {
     switch (action.type) {
         case actionSidenav.SHOW:
             return true;
