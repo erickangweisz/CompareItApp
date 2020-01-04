66859e9a7777869ca8b322d5a30be0f69235b6fd
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-04 16:53:40 +0000

    refactor and rename search.service to product.service

--- src/app/_services/product/product.service.ts
+++ src/app/_services/product/product.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
  
 @Injectable({
   providedIn: 'root'
 })
-export class SearchService {
+export class ProductService {
   apiUrl: string;
   
   constructor(private http: HttpClient) {
     this.apiUrl = environment.apiURL;
   }
 
-  getProducts(urlParams: string) {
-    return this.http.get(`${this.apiUrl}/search${urlParams}`);
+  get(params: HttpParams) {
+    return this.http.get(this.apiUrl, {
+      params: params,
+      observe: 'response'
+    })
   }
 }
