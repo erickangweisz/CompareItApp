435bddf1b6a9e5357b4bf58cb4bc01f5a4c70baf
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-04 16:57:49 +0000

    move logic to load products from search-component to search-form component

--- src/app/_components/search-form/search-form.component.ts
+++ src/app/_components/search-form/search-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpParams, HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+import { Location } from '@angular/common';
+import { ProductService } from 'src/app/_services/product/product.service';
 
 @Component({
   selector: 'app-search-form',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.scss']
 })
 export class SearchFormComponent implements OnInit {
+  apiUrl: string;
+  showSpinner: boolean;
+  products: [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private productService: ProductService,
+    private location: Location) {
+    this.apiUrl = environment.apiURL;
+    this.showSpinner = false;
   }
 
+  ngOnInit() {
+    this.searchProducts();
+  }
+
+  searchProducts() {
+    this.showSpinner = true;
+
+    let params = new HttpParams();
+    params = params.append('shops[]', 'pccomponentes')
+                   .append('term', 'asus zenbook')
+                   .append('nProductsPerShop', '3')
+    this.productService.get(params)
+      .toPromise()
+      .then(response => {
+        //this.location.replaceState(response.url);
+        this.products = response.body['resp'].products;
+        this.showSpinner = false;
+      })
+      .catch(console.log);
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,25 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {SearchService} from '../_services/search/search.service';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
-  products = [];
-
-  constructor(private searchService: SearchService) {}
-
-  ngOnInit() {
-    const urlParams = location.search;
-
-    if (urlParams) {
-      this.searchService.getProducts(urlParams).subscribe(res => {
-        this.products = res['resp'].products;
-      });
-    } else {
-      console.log('No params provided');
-    }
-  }
+export class SearchComponent {
+  constructor() {}
 }
