c605e0c11e8e267735b5121b2d8bf8142284f5bc
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-05 17:43:24 +0000

    import and integrate MatCardModule

--- src/app/_modules/material/material.module.ts
+++ src/app/_modules/material/material.module.ts
@@ -8,7 +8,8 @@ import { MatButtonModule,
         MatSidenavModule,
         MatFormFieldModule,
         MatInputModule,
-        MatSliderModule } from '@angular/material';
+        MatSliderModule,
+        MatCardModule } from '@angular/material';
 
 const material = [
   MatButtonModule,
@@ -20,7 +21,8 @@ const material = [
   MatSidenavModule,
   MatFormFieldModule,
   MatInputModule,
-  MatSliderModule
+  MatSliderModule,
+  MatCardModule
 ];
 
 @NgModule({
