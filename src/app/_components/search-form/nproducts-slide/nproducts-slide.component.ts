3696a3c8d738295e766cb71033ff2b97166d4978
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-05 15:54:16 +0000

    generate nproduct-slide component and implement it

---
+++ src/app/_components/search-form/nproducts-slide/nproducts-slide.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducers';
+import { NProductsSlideAction } from 'src/app/store/nproducts-slide/nproducts-slide.actions';
+
+@Component({
+  selector: 'app-nproducts-slide',
+  templateUrl: './nproducts-slide.component.html',
+  styleUrls: ['./nproducts-slide.component.scss']
+})
+export class NproductsSlideComponent {
+  constructor(private store: Store<AppState>) {}
+
+  numberOfProductsSelector(nProducts: number): number {
+    if (nProducts > 10)
+      return nProducts;
+
+    this.setNProductsToStore(nProducts);
+    return nProducts;
+  }
+
+  setNProductsToStore(nProducts: number): void {
+    let action = new NProductsSlideAction(nProducts);
+    this.store.dispatch(action);
+  }
+}
