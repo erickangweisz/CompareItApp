957563ea5d6afd79a1d2037a47bfc128d5ee3bfa
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-05 16:31:24 +0000

    import searchForm component to call searchProducts function

--- src/app/_components/search-form/term-input/term-input.component.ts
+++ src/app/_components/search-form/term-input/term-input.component.ts
@@ -3,16 +3,24 @@ import { TermInputAction } from 'src/app/store/term-input/term-input.actions';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store/app.reducers';
 
+import { SearchFormComponent } from 'src/app/_components/search-form/search-form.component';
+
 @Component({
   selector: 'app-term-input',
   templateUrl: './term-input.component.html',
   styleUrls: ['./term-input.component.scss']
 })
 export class TermInputComponent {
-  constructor(private store: Store<AppState>) {}
+  constructor(
+    private store: Store<AppState>,
+    private searchForm: SearchFormComponent) {}
 
   setTermToStore(termValue: string): void {
     let action = new TermInputAction(termValue);
     this.store.dispatch(action);
+  }
+
+  searchProducts(): void {
+    this.searchForm.searchProducts();
   }
 }
