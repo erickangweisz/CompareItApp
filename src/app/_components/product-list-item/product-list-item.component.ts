ebebe47cf653892f04a988a35bbe221be7007699
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2020-01-07 14:57:28 +0000

    implement a dynamical url of shop image

--- src/app/_components/product-list-item/product-list-item.component.ts
+++ src/app/_components/product-list-item/product-list-item.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list-item',
   templateUrl: './product-list-item.component.html',
   styleUrls: ['./product-list-item.component.scss']
 })
-export class ProductListItemComponent {
+export class ProductListItemComponent implements OnInit {
+  shopname: string;
   @Input() product;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.shopname = this.product['shopname']
+  }
 }
